3f60cd47a15951bb7cab38b8d82f8447f695f7e7
katie_w <katie.wiese11887@gmail.com>
2026-04-25 14:39:09 -0500

    Updated number of remaining guess

--- js/script.js
+++ js/script.js
@@ -7,8 +7,23 @@ const remainingGuessesSpan = document.querySelector(".remaining span");
 const messages = document.querySelector(".messages");
 const playItAgainButton = document.querySelector(".play-it-again");
 
-const word = "magnolia";
+let word = "magnolia";
 const guessedLetters = [];
+let remainingGuesses = 8;
+
+const getWord = async function () {
+    const response = await
+    fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+
+    const words = await response.text();
+    const wordArray = words.split("\n");
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
+    word = wordArray[randomIndex].trim();
+
+    placeholder(word);
+};
+
+getWord();
 
 const placeholder = function (word) {
     const placeholderLetters = [];
@@ -20,8 +35,6 @@ const placeholder = function (word) {
 
     wordInProgress.innerText = placeholderLetters.join("");
 };
-
-placeholder(word);
 
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
@@ -65,6 +78,7 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        updateGuessesRemaining(guess);
         showGuessedLetters();
         updateWordInProgress(guessedLetters);
     }
@@ -98,6 +112,29 @@ const updateWordInProgress = function (guessedLetters) {
     wordInProgress.innerText = revealWord.join("");
     checkIfWon();
 };
+const updateGuessesRemaining = function (guess) {
+    
+    const upperWord = word.toUpperCase();
+
+    if (!upperWord.includes(guess)) {
+        messages.innerText = `That's a negative, Ghost Rider! No ${guess} here.`;
+        remainingGuesses -= 1;
+
+    } else {
+        messages.innerText = `Nailed it!`;
+    }
+
+    if (remainingGuesses === 0) {
+        messages.innerHTML = `Sorry! <span class="highlight"> ${word}</span> won this round.`;
+
+    } else if (remainingGuesses === 1) {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
+
+    } else {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+    }
+};
+
 
 const checkIfWon = function () {
 
@@ -105,4 +142,6 @@ const checkIfWon = function () {
         messages.classList.add("winner");
         messages.innerHTML = `<p class="highlight">You did it! Winner Winner Chicken Dinner!</p>`;
     }
+
 };
+
